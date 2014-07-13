2f2296c37928cbc2a8910d319e68be5512df7935
Dion Hyman <hymandx@Dions-MacBook-Pro.local>
2014-07-13 13:55:12 +0700

    Updates

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-plot3 <- function (){
+plot4 <- function (){
   
   # Load the source file
   df <- read.csv('figure/household_power_consumption.txt',sep=";",stringsAsFactors = FALSE)
@@ -9,7 +9,10 @@ plot3 <- function (){
                                format="%d/%m/%Y %H:%M:%S")
   
   # Make plot into PNG file
-  png("plot3.png", height=480, width=480)
+  png("plot4.png", height=480, width=480)
+  par(mfrow = c(2,2))
+  plot(dfsmall$Global_active_power~dfsmall$mydate,type="l",xlab="",ylab="Global Active Power")
+  plot(dfsmall$Voltage~dfsmall$mydate,type="l",xlab="",ylab="Voltage")
   plot(dfsmall$Sub_metering_1~dfsmall$mydate,type="l",xlab="",ylab="Energy sub metering")
   lines(dfsmall$Sub_metering_2~dfsmall$mydate,type="l",col="red")
   lines(dfsmall$Sub_metering_3~dfsmall$mydate,type="l",col="blue")
@@ -19,5 +22,7 @@ plot3 <- function (){
          col = c(
            "black","red","blue"), 
          legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  plot(dfsmall$Global_reactive_power~dfsmall$mydate,type="l",xlab="",ylab="Global_reactive_power")
+  
   dev.off()
 }
